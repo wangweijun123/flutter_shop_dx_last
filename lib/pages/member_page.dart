3ec4071c8c2b472c388f0ae9f82caa9c8944d160
jntm <262448350@qq.com>
2024-10-14 01:45:38 +0800

    会员

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -1,8 +1,54 @@
 import 'package:flutter/material.dart';
 
-class MemberPage extends StatelessWidget {
+class MemberPage extends StatefulWidget {
+  @override
+  State<MemberPage> createState() => _MemberPageState();
+}
+
+class _MemberPageState extends State<MemberPage> {
   @override
   Widget build(BuildContext context) {
-    return Text("我的");
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('我的'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 50,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _itemCart(index);
+                  }),
+            ),
+            Container(
+              color: Colors.amber,
+              child: const Row(
+                children: [
+                  Text('底部'),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text('底部'),
+                        Text('底部'),
+                        Text('底部'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+
+  Text _itemCart(int index) {
+    return Text(
+      'index = $index',
+      style: const TextStyle(
+        fontSize: 30,
+      ),
+    );
   }
 }
