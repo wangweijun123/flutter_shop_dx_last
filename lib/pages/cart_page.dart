96af2f34143c07d7ba06b5c4e12a4d81d3441444
jntm <262448350@qq.com>
2024-10-14 01:29:28 +0800

    购物车列表

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,8 +1,56 @@
 import 'package:flutter/material.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
+  const CartPage({super.key});
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return Text("购物车");
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('购物车'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 50,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _itemCart(index);
+                  }),
+            ),
+            Container(
+              color: Colors.amber,
+              child: const Row(
+                children: [
+                  Text('底部'),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text('底部'),
+                        Text('底部'),
+                        Text('底部'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+
+  Text _itemCart(int index) {
+    return Text(
+      'index = $index',
+      style: const TextStyle(
+        fontSize: 30,
+      ),
+    );
   }
 }
